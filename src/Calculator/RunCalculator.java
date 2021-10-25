0ee7b096dbd267f337ad8868de6ad94c811bb889
i.kartashov <my@mail.ru>
2021-10-25 17:48:02 +0300

    Calculator with functions on buttons

--- src/Calculator/RunCalculator.java
+++ src/Calculator/RunCalculator.java
@@ -2,8 +2,6 @@ package Calculator;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class RunCalculator {
     public static void main(String[] args) {
@@ -11,53 +9,38 @@ public class RunCalculator {
         frame.setSize(300,300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel content = new JPanel(new VerticalLayout());
-        content.add(new TextField("text",10));
-
-        class ButAction implements ActionListener{
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        }
-        
+        content.add(new TextField("0",25));
         JPanel grid = new JPanel();
         GridLayout gLayout = new GridLayout(4,4,5,5);
         grid.setLayout(gLayout);
-        for (int i = 9; i > 6; i--){
-            grid.add(new JButton(String.valueOf(i)));
 
+        for (int i = 9; i > 6; i--){
+            addAction((JButton) grid.add(new JButton(String.valueOf(i))));
         }
-        grid.add(new JButton("/"));
+        addAction((JButton) grid.add(new JButton("/")));
         for (int i = 6; i > 3; i--){
-            grid.add(new JButton(String.valueOf(i)));
+            addAction((JButton) grid.add(new JButton(String.valueOf(i))));
         }
-        grid.add(new JButton("*"));
+        addAction((JButton) grid.add(new JButton("*")));
         for (int i = 3; i > 0; i--){
-            grid.add(new JButton(String.valueOf(i)));
+            addAction((JButton) grid.add(new JButton(String.valueOf(i))));
         }
-        grid.add(new JButton("-"));
-        grid.add(new JButton("0"));
-        grid.add(new JButton("+"));
-        grid.add(new JButton("^x"));
-        grid.add(new JButton("="));
-        //frame.getContentPane().add(grid);
+        addAction((JButton) grid.add(new JButton("-")));
+        addAction((JButton) grid.add(new JButton("0")));
+        addAction((JButton) grid.add(new JButton("+")));
+        addAction((JButton) grid.add(new JButton("^x")));
+        resultAction((JButton) grid.add(new JButton("=")));
 
         content.add(grid);
-
         frame.setContentPane(content);
-        //frame.pack();
         frame.setVisible(true);
-
-
     }
 
-    public void addAction(JButton button){
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Test" + button.getText());
-            }
-        });
+    public static void addAction(JButton button){
+        button.addActionListener(e -> System.out.println("Test" + button.getText()));
+    }
+
+    public static void resultAction(JButton button){
+        button.addActionListener(e -> System.out.println("result" + button.getText()));
     }
 }
