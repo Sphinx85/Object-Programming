39e5879c75a6197d9aac57130109d2b1237b2cbf
i.kartashov <my@mail.ru>
2022-01-10 14:42:16 +0300

    Выполненное задание. Сделана поправка на задание. Теперь каждый метод создает свой массив.
    Необходимо написать два метода, которые делают следующее:
    1) Создают одномерный длинный массив;
    2) Заполняют этот массив единицами;
    3) Засекают время выполнения;
    4) Проходят по всему массиву и для каждой ячейки считают новое значение по формуле:
    arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
    5) Проверяется время окончания метода;
    6) В консоль выводится время работы;
    Отличие первого метода от второго:
    Первый просто бежит по массиву и вычисляет значения.
    Второй разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает эти массивы обратно в один.

--- src/Lesson16MultiThread/MainRun.java
+++ src/Lesson16MultiThread/MainRun.java
@@ -2,32 +2,38 @@ package Lesson16MultiThread;
 
 import java.util.Arrays;
 
-public class MainRun {
+public class MainRun  extends Thread{
     /**
-     * Задаем переменные, которые будут всегда исползоваться.
+     * Задаем переменные, которые будут всегда использоваться.
      */
     public static final int size = (int) (Math.random() * 10000000);
     public static final int halfSize = size/2;
-    public static Float [] halfArrayOne = new Float[halfSize];
-    public static Float [] halfArrayTwo = new Float[halfSize];
-    public static Float[] mainArray = new Float[size];
 
+    /**
+     * Класс, запускающий первый метод в отдельном потоке
+     */
     public static class MethodOne extends MainRun implements Runnable{
 
         @Override
         public void run() {
-            methodOne(mainArray);
+            methodOne();
         }
     }
 
+    /**
+     * Класс, запускающий второй метод в отдельном потоке
+     */
     public static class MethodTwo extends MainRun implements Runnable{
 
         @Override
         public void run() {
-            methodTwo(mainArray);
+            methodTwo();
         }
     }
 
+    /**
+     * Класс для запуска пересчета массива в потоке
+     */
     public static class CalculateArrayThread extends MainRun implements Runnable{
 
         private final Float[] half;
@@ -44,7 +50,10 @@ public class MainRun {
         }
     }
 
-    /*public static class ArrayConnect implements Runnable{
+    /**
+     * Класс для склейки массивов в потоке
+     */
+    public static class ArrayConnect extends Thread{
         Float[] half1;
         Float[] half2;
 
@@ -54,35 +63,55 @@ public class MainRun {
         }
 
         @Override
-        public synchronized void run() {
-            mainArray = arrayConnect(half1,half2);
+        public void run() {
+            arrayConnect(half1,half2);
         }
-    }*/
+    }
 
     public static void main(String[] args) {
-        arrayFully(mainArray);
         new Thread(new MethodOne()).start();
         new Thread(new MethodTwo()).start();
     }
     /**
      * Метод первый
-     * Вычисляет новые значения ячеек
+     * вычисляет новые значения ячеек
      */
-    public static void methodOne(Float[] array){
+    public static void methodOne(){
+        Float[] array = new Float[size];
+        arrayFully(array);
         long before = System.currentTimeMillis();
         arrayCalculator(array);
         System.out.println("Время пересчета ячеек первым методом: " + (System.currentTimeMillis() - before));
     }
     /**
      * Метод второй
-     * Разбивает массив на два массива, в двух потоках высчитывает новые значения
+     * разбивает массив на два массива, в двух потоках высчитывает новые значения
      * и потом склеивает эти массивы обратно в один.
      */
-    public static void methodTwo(Float [] array){
-        arrayDivision(array);
-        new Thread(new CalculateArrayThread(halfArrayOne)).start();
-        new Thread(new CalculateArrayThread(halfArrayTwo)).start();
-        mainArray = arrayConnect(halfArrayOne,halfArrayTwo);
+    public static void methodTwo(){
+        Float[] array = new Float[size];
+        Float[] halfArrayOne = new Float[halfSize];
+        Float[] halfArrayTwo = new Float[halfSize];
+        arrayFully(array);
+        long before = System.currentTimeMillis();
+        arrayDivision(array,halfArrayOne,halfArrayTwo);
+        Thread calculateOne = new Thread(new CalculateArrayThread(halfArrayOne));
+        Thread calculateTwo = new Thread(new CalculateArrayThread(halfArrayTwo));
+        calculateOne.start();
+        calculateTwo.start();
+        Thread connectArrays = new Thread(new ArrayConnect(halfArrayOne,halfArrayTwo));
+        while (true){
+            if (!(calculateOne.isAlive() || calculateTwo.isAlive())){
+                connectArrays.start();
+                break;
+            }
+        }
+        while (true){
+            if (!connectArrays.isAlive()){
+                System.out.println("Время пересчета ячеек вторым методом: " + (System.currentTimeMillis() - before));
+                break;
+            }
+        }
     }
     /**
      * Метод подсчета значений
@@ -95,7 +124,7 @@ public class MainRun {
     /**
      * Медод разделения массива на два
      */
-    public static void arrayDivision(Float[] array){
+    public static void arrayDivision(Float[] array, Float[] halfArrayOne, Float[] halfArrayTwo){
         long before = System.currentTimeMillis();
         System.arraycopy(array,0, halfArrayOne,0,halfSize);
         System.arraycopy(array,halfSize, halfArrayTwo,0,halfSize);
@@ -104,13 +133,12 @@ public class MainRun {
     /**
      * Метод склейки массивов
      */
-    public static Float[] arrayConnect(Float[] half1, Float[] half2){
+    public static void arrayConnect(Float[] half1, Float[] half2){
         long before = System.currentTimeMillis();
         Float[] array = new Float[size];
         System.arraycopy(half1,0,array,0,halfSize);
         System.arraycopy(half2,0,array,halfSize,halfSize);
         System.out.println("Время склейки массива вторым методом: " + (System.currentTimeMillis() - before));
-        return array;
     }
     /**
      * Метод заполнения массива
